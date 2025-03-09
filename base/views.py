05740b1f3aa3adb3a8fe8441ab1cf5caef2ebb99
siddhartha-p <siddhartha.pradhan.08@gmail.com>
2025-03-09 18:49:43 +0545

    added Topic and Messages model

--- base/views.py
+++ base/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 # Create your views here.
 
-rooms=[
-    {'id':1, 'name':'lets learn Python'},
-    {'id':2, 'name':'lets learn javascript'},
-    {'id':3, 'name':'lets learn C#'}
-]
+# rooms=[
+#     {'id':1, 'name':'lets learn Python'},
+#     {'id':2, 'name':'lets learn javascript'},
+#     {'id':3, 'name':'lets learn C#'}
+# ]
 
 def home(request):
+    rooms=Room.objects.all()
     context={'rooms':rooms}
     return render(request,'home.html',context)
 
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id']==int(pk):
-            room=i
+    room=Room.objects.get(id=pk)
     context={'room':room}
     return render(request,'room.html',context)
