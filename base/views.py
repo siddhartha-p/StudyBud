1e9bfff1011d6a00596e5250cd2251722f2f421f
siddhartha-p <siddhartha.pradhan.08@gmail.com>
2025-03-14 20:55:09 +0545

    login and logout feature added

--- base/views.py
+++ base/views.py
@@ -32,6 +32,14 @@ def loginPage(request):
     return render(request,'base/login_register.html',context)
 
 
+def logoutUser(request):
+    
+    logout(request) #deletes the session from the browser
+    return redirect('home')
+
+    
+
+
 def home(request):
     q=request.GET.get('q') if request.GET.get('q') != None else ''
     rooms=Room.objects.filter(
