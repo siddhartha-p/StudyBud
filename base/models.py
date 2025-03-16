db25dc83cda3108be39e9bba2be4910acf20fe17
siddhartha-p <siddhartha.pradhan.08@gmail.com>
2025-03-16 14:05:32 +0545

    particapation feature added

--- base/models.py
+++ base/models.py
@@ -15,6 +15,7 @@ class Room(models.Model):
     topic=models.ForeignKey(Topic,on_delete=models.SET_NULL,null=True)
     name=models.CharField(max_length=100)
     decription=models.TextField(null=True,blank=True)
+    participants=models.ManyToManyField(User,related_name='participants',blank=True)
     updated=models.DateTimeField(auto_now=True)
     created=models.DateTimeField(auto_now_add=True)
     
@@ -25,7 +26,7 @@ class Room(models.Model):
         ordering=['-created','-updated']
     
 class Message(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE  )
     room=models.ForeignKey(Room,on_delete=models.CASCADE)
     body=models.TextField()
     updated=models.DateTimeField(auto_now=True)
